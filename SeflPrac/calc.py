dc4f8bb3e9d5d3271c997cfa10488f22e1d253d0
Joshwa <joshwafernandes74@gmail.com>
2024-01-08 19:54:06 +0530

    added new  files 08,1,24

---
+++ SeflPrac/calc.py
@@ -0,0 +1,27 @@
+qes = (input())
+ls = qes.split()
+
+opls = ["+", "-", "*", "/", "%"]
+
+
+
+try:
+    if(int(ls[0]).isdigit and ls[2]!= "0"  ):
+        if(ls[1] == opls[0]):
+            summ = int(ls[0]) + int(ls[2])
+        elif(ls[1] == opls[1]):
+            summ = int(ls[0]) - int(ls[2])
+        elif(ls[1] == opls[2]):
+            summ = int(ls[0]) * int(ls[2])
+        elif(ls[1] == opls[3]):
+            summ = int(ls[0])/int(ls[2])
+        elif(ls[1] == opls):
+            summ = int(ls[0])%int(ls[2])
+        print(summ) 
+    else:
+        print("Invalid char")
+except:
+    print("invalid input")
+   
+
+
